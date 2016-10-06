be73d7c4757f239eaef86476d40848ff59eae869
Gerard Ng <gerardng@users.noreply.github.com>
2016-10-05 17:45:34 -0700

    Delete justone.php

--- application/views/justone.php
+++
@@ -1,7 +0,0 @@
-<div class="row">
-    <div class="span3"><img src="/pix/{pic}" title="{name}"/></div>
-    <div class="span8 offset1">
-        <p class="lead">{description}</p><br/>
-        <p class="text-right">{name}</p>
-    </div>
-</div>
