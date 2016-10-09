6bbb4ca92f188a982c7b8ab32a51dad7b270abf2
Tracy <tracycuiliu815@gmail.com>
2016-10-09 13:03:31 -0700

    fix the layout of inventory_view
    Signed-off-by: Tracy <tracycuiliu815@gmail.com>

--- application/views/Inventory_view.php
+++ application/views/Inventory_view.php
@@ -7,15 +7,15 @@
         
         <div class="form-group">
         <label for="ReceivingUnit">Receiving Unit:</label>
-        <input type="text" class ="form-control" value="{receivingNo} {receivingUnit}"><br/>
+        <input type="text" class ="form-control textbox" value="{receivingNo} {receivingUnit}"><br/>
         </div>
         <div class="form-group">
         <label for="ReceivingCost">Receiving Cost:</label>
-        <input type="text" class ="form-control" value="${receivingCost}"><br/>
+        <input type="text" class ="form-control textbox" value="${receivingCost}"><br/>
         </div>
         <div class="form-group">
         <label for="Quantity">Quantity:</label>
-        <input type="text" class ="form-control" value="{quantities}"><br/>
+        <input type="text" class ="form-control textbox" value="{quantities}"><br/>
         </div>       
     </form>
     </div>              
