973130205424c0f445511975525b3cb71b773608
Kwanchanok Sonsoi <kwanchanok.mai@gmail.com>
2016-10-09 11:22:15 -0700

    cheanged table style on Salesmenu.php

--- application/views/Salesmenu.php
+++ application/views/Salesmenu.php
@@ -2,18 +2,15 @@
 
     <table class = "sales">
         <tr>
-            <th>Image</th>
-            <th>Name</th>
-            <th>Price</th>
-            <th>Description</th>
-            <th>Stock</th>
-        </tr>
-        <tr>
-            <td> <div class=""><img src="/pix/{pic}" title="{name}"/></a></div></td>
-            <td><p class="lead">{name}</p></td>
-            <td> <p class="lead">${price}</p><br/></td>
-            <td> <p class="text-left">{description}</p></td>
-            <td><p class="text-left">{order} items</p></td>
+
+                <div class="thumbnail">
+            <tr> <div class=""><img src="/pix/{pic}" title="{name}"/></a></div></tr>
+                </div>
+
+            <tr><p class="text-left">Name: {name}</p></tr>
+            <tr> <p class="text-left">Price: ${price}</p><br/></tr>
+            <tr> <p class="text-left">Description: {description}</p></tr>
+            <tr><p class="text-left">Stock Available: {order}items</p></tr>
         </tr>
 
     </table>
